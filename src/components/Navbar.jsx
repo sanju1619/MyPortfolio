3d7130f3170d1a4d2424973b150eb763f5e8bfda
sanju1619 <git@stackblitz.com>
2025-02-17 11:00:11 +0530

    Initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './Navbar.css'; // Import CSS file
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <div className="logo">
+        <span className="icon">🅿️</span>
+        <span className="brand">Personal</span>
+      </div>
+
+      <ul className="nav-links">
+        <li>
+          <a href="#about">About Me</a>
+        </li>
+        <li>
+          <a href="#skills">Skills</a>
+        </li>
+        <li>
+          <a href="#projects">Project</a>
+        </li>
+        <li>
+          <a href="#contact">Contact Me</a>
+        </li>
+      </ul>
+
+      <button className="resume-btn">
+        Resume <span className="download-icon">⬇️</span>
+      </button>
+    </nav>
+  );
+};
+
+export default Navbar;
